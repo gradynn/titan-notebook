13b69f0b71813c900d087bf7ee57c152ad122b0d
Gradyn Nagle <gradyndnagle@outlook.com>
2024-10-23 17:16:26 -0700

    added Tailwind for easy styling

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,12 +1,13 @@
 // src/renderer.ts
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 // Create a simple React component
 const App: React.FC = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-gray-700 flex items-center justify-center">
+      <h1 className="text-sm font-bold text-blue-500">Hello from React + Tailwind CSS!</h1>
     </div>
   );
 };
